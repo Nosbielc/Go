8d91a237b28cf764e56dcb066bc25eb4ca5c41b1
Cleibson Gomes da Silva <cleibson.gomes.da.silva@gmail.com>
2019-07-17 16:57:42 -0300

    Create Variavel.go

---
+++ fundamentos/variaveis/Variavel.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var (
+	//Endereco é um valor importante e tem de ser publico
+	Endereco string //endereco = ""
+	//Telefone é um valor importante para a nossa aula
+	telefone            string = "+55081999999999"
+	quantidade, estoque int     //quantidade = 0
+	comprou             bool    //comprou = false
+	valor               float64 //valor = 0.00
+	palavras            rune
+)
+
+func main() {
+
+	teste := "Valor de teste"
+	fmt.Printf("Endereço: %s\r\n", Endereco)
+	fmt.Printf("Telefone: %s\r\n", telefone)
+	fmt.Printf("Quantidade: %d\r\n", quantidade)
+	fmt.Printf("Comprou: %v\r\n", comprou)
+	fmt.Printf("Palavras: %v\r\n", palavras)
+	fmt.Printf("O valor de teste é: %v\r\n", teste)
+
+}
